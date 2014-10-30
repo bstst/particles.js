ca539ed6c82d5843fc4e4470b8432bad61171420
Davide Mendolia <idaf1er@gmail.com>
2014-10-30 15:44:18 +0000

    From SVG to Canvas inital code

--- demo/js/app.js
+++ demo/js/app.js
@@ -4,21 +4,58 @@
 /* config dom id */
 //particlesJS('dom-id');
 
+//rect reader
+var Position = function (x, y) {
+    this.x = x | 0.0;
+    this.y = y | 0.0;
+};
+
+var Offset = function (x, y) {
+    this.x = x | 0.0;
+    this.y = y | 0.0;
+};
+
+svgGroup = document.getElementById('Group1');
+rects = svgGroup.getElementsByTagName('rect');
+
+var raw_particles = [];
+var offset1 = new Offset(36.0, 0);
+
+for (var i = 0; i < rects.length; i++) {
+    raw_particles.push(new Position(parseFloat(rects[i].getAttribute('x')) + offset1.x, parseFloat(rects[i].getAttribute('y')) + offset1.y));
+}
+
+svgGroup = document.getElementById('Group2');
+rects = svgGroup.getElementsByTagName('rect');
+
+var offset2 = new Offset(1.0, 150.0);
+
+for (var i = 0; i < rects.length; i++) {
+    raw_particles.push(new Position(parseFloat(rects[i].getAttribute('x')) + offset2.x, parseFloat(rects[i].getAttribute('y')) + offset2.y));
+}
+
+
 /* config dom id (optional) + config particles params */
 particlesJS('particles-js', {
-	canvas: {
-		color_hex_bg: '#15414e',
-		opacity: 1
-	},
-	particles: {
-	    color_hex: '#fff',
-		opacity: 1,
-		size: 2.5,
-		size_random: true,
-		nb: 200,
-		anim: {
-			speed: 2
-		}
-	},
-	retina_detect: true
+    canvas: {
+        color_hex_bg: '#15414e',
+        opacity: 1
+    },
+    particles: {
+        color: '#000',
+        shape: 'edge',
+        opacity: 1,
+        size: 2,
+        size_random: false,
+        raw_particles: raw_particles,
+        anim: {
+            speed: 1
+        },
+        line_linked: {
+            color: '#000',
+            distance: 150
+        }
+    },
+    retina_detect: true
 });
+
